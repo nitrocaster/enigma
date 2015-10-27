7bfd1d5e5b7052fe32c080399a71323d78a4fe28
nitrocaster <paul-kv@yandex.ru>
2015-10-28 01:41:39 +0300

    Implement command line interface.

--- src/enigma/config.h
+++ src/enigma/config.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define CONFIG_TRANSFORM_BUFFER_SIZE 4*1024
--- src/enigma/main.c
+++ src/enigma/main.c
@@ -1,7 +1,121 @@
 #include "config.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 #include "enigma.h"
+#include "rotor_config.h"
+
+static void print_usage()
+{ puts("usage: enigma <key file> <source file> <destination file>"); }
+
+static int file_exists(const char *path)
+{
+    struct stat buffer;
+    buffer.st_size;
+    return !stat(path, &buffer);
+}
+
+typedef struct
+{
+    uint8_t count;
+    uint8_t positions[];
+} enigma_rotor_cfg_t;
+
+typedef struct
+{
+    uint8_t count;
+    byte_pair_t pairs[];
+} enigma_plugboard_cfg_t;
+
+#define MSG_INVALID_KEY_FILE "invalid key file."
+#define ROTOR_COUNT 4
+
+static enigma_rotor_t rotor_buf[ROTOR_COUNT];
+
+static void load_key_file(const char *path, enigma_rotor_cfg_t **r_cfg,
+    enigma_plugboard_cfg_t **pb_cfg)
+{
+    FILE *key = NULL;
+    if (!file_exists(path))
+    {
+        puts("key file not found.");
+        exit(1);
+    }
+    key = fopen(path, "rb");
+    uint8_t count;
+    if (fread(&count, 1, 1, key)!=1 || count!=ROTOR_COUNT)
+    {
+        puts(MSG_INVALID_KEY_FILE);
+        exit(1);
+    }
+    *r_cfg = malloc(sizeof(enigma_rotor_cfg_t)+count);
+    (*r_cfg)->count = count;
+    if (fread((*r_cfg)->positions, 1, count, key)!=count)
+    {
+        puts(MSG_INVALID_KEY_FILE);
+        exit(1);
+    }
+    if (fread(&count, 1, 1, key)!=1)
+    {
+        puts(MSG_INVALID_KEY_FILE);
+        exit(1);
+    }
+    *pb_cfg = malloc(sizeof(enigma_plugboard_cfg_t)+count);
+    (*pb_cfg)->count = count;
+    if (count &&
+        fread((*pb_cfg)->pairs, sizeof(byte_pair_t), count, key)!=count)
+    {
+        puts(MSG_INVALID_KEY_FILE);
+        exit(1);
+    }
+    fclose(key);
+}
 
 int main(int argc, char *argv[])
 {
+    if (argc!=4)
+    {
+        print_usage();
+        return 1;
+    }
+    const char *key_path = argv[1];
+    const char *src_path = argv[2];
+    const char *dst_path = argv[3];
+    enigma_rotor_cfg_t *r_cfg = NULL;
+    enigma_plugboard_cfg_t *pb_cfg = NULL;
+    load_key_file(key_path, &r_cfg, &pb_cfg);
+    enigma_rotor_init(rotor_buf+0, r_cfg->positions[0],
+        ENIGMA_ROTOR_TYPE_PLAIN, r_subst_0, NULL);
+    enigma_rotor_init(rotor_buf+1, r_cfg->positions[1],
+        ENIGMA_ROTOR_TYPE_CUSTOM, r_subst_1, r_notches_1);
+    enigma_rotor_init(rotor_buf+2, r_cfg->positions[2],
+        ENIGMA_ROTOR_TYPE_CUSTOM, r_subst_2, r_notches_1);
+    enigma_rotor_init(rotor_buf+3, r_cfg->positions[3],
+        ENIGMA_ROTOR_TYPE_CUSTOM, r_subst_refl, r_notches_1);
+    enigma_state_t *enigma = enigma_state_init(
+        rotor_buf, ROTOR_COUNT, pb_cfg->pairs, pb_cfg->count);
+    FILE *src = fopen(src_path, "rb");
+    if (!src)
+    {
+        puts("can't open source file.");
+        return 1;
+    }
+    FILE *dst = fopen(dst_path, "wb+");
+    if (!dst)
+    {
+        puts("can't open destination file.");
+        return 1;
+    }
+    uint8_t buf[CONFIG_TRANSFORM_BUFFER_SIZE];
+    size_t r_sz = 0;
+    while ((r_sz = fread(buf, 1, CONFIG_TRANSFORM_BUFFER_SIZE, src)))
+    {
+        enigma_state_transform(enigma, buf, buf, r_sz);
+        fwrite(buf, 1, r_sz, dst);
+    }
+    fclose(src);
+    fclose(dst);
+    enigma_state_deinit(enigma);
     return 0;
 }
